c16c26aea8edb7e6d811fc3c514834a4b22e618f
Legna261 <127366879+Legna261@users.noreply.github.com>
2023-03-11 19:43:35 -0600

    Add files via upload

--- entregable imc.py
+++ entregable imc.py
@@ -1,10 +1,20 @@
 ## Programa para calcular el IMC de una persona
 
-nombre = input("introduce tu nombre:") #solicita el nombre de la persona.
-print ("bienvenido:" ,nombre) # da la bienvenida con el nombre de usuario
-edad = int(input("introduce tu edad:")) # solicitamos la edad 
+nombre = input("introduce tu nombre: ") #solicita el nombre de la persona.
+if not nombre:
+        print("Este campo es obligatorio, favor de llenarlo")
+
+apellidopaterno= input("introduce tu apellido paterno: ") # solicita el apellido paterno del usuario
+if not apellidopaterno:        
+        print("Este campo es obligatorio, favor de llenarlo")
+        
+apellidomaterno= input("introduce tu apellido materno: ") # solicita el apellido materno del usuario
+if not apellidomaterno:
+        print("Este campo es obligatorio, favor de llenarlo")
+
+print ("bienvenido:" ,nombre , apellidopaterno , apellidomaterno)  # da la bienvenida con el nombre de usuario
+
+edad = int(input("introduce tu edad:")) # solicitamos la edad        
 altura = float(input("introduce tu altura:")) #solicitamos su altura
 peso = float(input("introduce tu peso en kilogramos:")) #solicitamos su peso en kilogramos 
-print ("su IMC= ", peso/altura**2) #imprimimos su indice de masa corporal
-
-
+print ( nombre , apellidopaterno , apellidomaterno , "su IMC= ", peso/altura**2) #imprimimos el nombre del usuario acompañado del calculo de su IMC.
